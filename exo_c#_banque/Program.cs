301c82e42542f4b2965f67c7ab5d0afcf7444049
Cassandra1978 <c.loger@campussaintaspais.fr>
2026-09-16 20:03:24 +0200

    exo1

--- exo_c#_banque/Program.cs
+++ exo_c#_banque/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿ecimal solde = 1000m;
+decimal montant = 250m;
+if (montant > 0)
+{
+solde += montant;
+}
+else if (montant < 0)
+{
+ decimal debit =- montant;
+ if(debit <= solde)
+ {
+ solde =- debit;
+ }
+ else
+ {
+    Console.WriteLine("Solde Insuffisant");
+ }
+} 
+Console.WriteLine($"Solde: {solde}");
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
